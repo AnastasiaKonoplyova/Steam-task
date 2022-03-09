5ff4ad265f4ec1f11a94ebe8dc638475704c525a
a.konoplyova <nastya.konoplyova@gmail.com>
2022-03-09 21:02:24 +0300

    create startPages

---
+++ src/main/java/MainPage.java
@@ -0,0 +1,15 @@
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("https://store.steampowered.com/")
+public class MainPage extends PageObject{
+
+    @FindBy(xpath = "//a[contains(@href,'/category/action/')]")
+    private WebElementFacade actionLn;
+
+    public void openActionPage(){
+        actionLn.click();
+    }
+}
