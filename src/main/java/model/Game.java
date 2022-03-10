e6b1e542cdf7c575f0373f2eba37d85aa635b5f2
a.konoplyova <nastya.konoplyova@gmail.com>
2022-03-10 21:31:03 +0300

    create model class

---
+++ src/main/java/model/Game.java
@@ -0,0 +1,12 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class Game {
+
+    private String name;
+    private String sale;
+    private String finalPrice;
+    private String originPrice;
+}
