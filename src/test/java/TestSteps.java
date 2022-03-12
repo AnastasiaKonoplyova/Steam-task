d23396c4abbdcf43da762d700927f29149410c56
a.konoplyova <nastya.konoplyova@gmail.com>
2022-03-12 12:04:42 +0300

    add test steps

--- src/test/java/TestSteps.java
+++ src/test/java/TestSteps.java
@@ -1,11 +1,21 @@
+import model.Game;
+import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Step;
+import org.openqa.selenium.WebDriver;
 import pages.ActionsGamesPage;
+import pages.AgeCheckPage;
 import pages.MainPage;
+import utils.GameUtil;
+
+import java.util.List;
 
 public class TestSteps {
 
     MainPage mainPage;
     ActionsGamesPage actionsGamesPage;
+    AgeCheckPage ageCheckPage;
+    @Managed(uniqueSession=true, driver="chrome")
+    WebDriver driver;
 
     @Step("Starting test, open main page and then actions games page")
     public void openActionsPage(){
@@ -18,15 +28,34 @@ public class TestSteps {
         actionsGamesPage.openTopSellers();
     }
 
-    @Step("Check if games with sales exist on the page")
-    public boolean checkSales(){
+    @Step("Check if games with sales exist on the page. If false -> turn page")
+    public void findSalesInGames(){
         actionsGamesPage.fillGameList();
-        return actionsGamesPage.ifSalesExit();
+        while (!actionsGamesPage.ifSalesExit()){
+            actionsGamesPage.turnMenu();
+            findSalesInGames();
+        }
     }
 
-    @Step()
-    public void findCheapestGame(){
-        actionsGamesPage.findCheapestGame(actionsGamesPage.getMaxSale());
-
+    @Step("Get games as objects")
+    public List<Game> getObjectGames(){
+       return actionsGamesPage.getGames();
     }
+
+    @Step("Find game with max sale")
+    public Game findGameBySale(List<Game> games){
+        return GameUtil.findGameWithMaxSale(games);
+    }
+
+    @Step("Open page with max Sale")
+    public void openGamePageBySale(Game game){
+        actionsGamesPage.openGamePage(game);
+    }
+
+    @Step("Check is needed page")
+    public boolean isAgeCheckPage(){
+        return ageCheckPage.isAgeCheckPage(driver.getCurrentUrl());
+    }
+
+
 }
