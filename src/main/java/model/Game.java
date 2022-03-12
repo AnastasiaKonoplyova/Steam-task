08790c00a436c4a3dceebfca719cd03728c77348
a.konoplyova <nastya.konoplyova@gmail.com>
2022-03-12 12:07:22 +0300

    add constructor annotation

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,12 +1,15 @@
 package model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Game {
 
     private String name;
-    private String sale;
+    private int sale;
     private String finalPrice;
     private String originPrice;
+
 }
