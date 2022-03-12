0a3866fa839f0c7efb0e31c0e87a5277b7c2ad3e
a.konoplyova <nastya.konoplyova@gmail.com>
2022-03-12 18:24:35 +0300

    finish ageCheck and get game data

--- src/main/java/pages/AgeCheckPage.java
+++ src/main/java/pages/AgeCheckPage.java
@@ -1,6 +1,52 @@
 package pages;
 
+import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.By;
 
 public class AgeCheckPage extends PageObject {
+
+    private final String urlPart = "agecheck";
+    private String selectParamLocator = "//select[contains(@id,'%s')]";
+    private String selectBlockLocator = "//div[contains(@class,'birthday_selector')]";
+    @FindBy(xpath = "//div[@class='agegate_text_container btns']/a[@href='#']")
+    private WebElementFacade viewPageBtn;
+    private WebElementFacade ageDaySelect;
+    private WebElementFacade ageMonthSelect;
+    private WebElementFacade ageYearSelect;
+
+    public boolean isAgeCheckPage(String url){
+        return url.contains(urlPart);
+    }
+
+    public void findAgeSelects(){
+        ageDaySelect = find(By.xpath(String.format(selectParamLocator, "Day")));
+        ageMonthSelect = find(By.xpath(String.format(selectParamLocator, "Month")));
+        ageYearSelect = find(By.xpath(String.format(selectParamLocator, "Year")));
+    }
+
+    public int findBoundaryAgeValue(){
+        return Integer.getInteger(ageYearSelect.getSelectOptions().get(0));
+    }
+
+    public void selectDay(int day){
+        ageDaySelect.selectByValue(String.valueOf(day));
+    }
+
+    public void selectMonth(String month){
+        ageMonthSelect.selectByValue(month);
+    }
+
+    public void selectYear(int year){
+        ageYearSelect.selectByValue(String.valueOf(year));
+    }
+
+    public void openGamePage(){
+        viewPageBtn.click();
+    }
+
+    public boolean isAgeSelectDisplayed(){
+        return find(By.xpath(selectBlockLocator)).isDisabled();
+    }
 }
--- src/main/java/pages/GamePage.java
+++ src/main/java/pages/GamePage.java
@@ -1,7 +1,39 @@
 package pages;
 
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.By;
+import utils.JSONReader;
+import utils.StringUtil;
 
 public class GamePage extends PageObject {
+
+    private String gameLocator = "//*[text()='Buy %s']";
+    private String gameParamLocator = "//following-sibling::div//div[contains(@class,'%s')]";
+    private WebElementFacade game;
+    private WebElementFacade installLn = find(By.xpath("//a[contains(@class,'installsteam')]"));
+
+    public void findGame(String gameName){
+        game = find(By.xpath(String.format(gameLocator, gameName)));
+    }
+
+    public String getGameOrigPrice(){
+        return game.findElement(By.xpath(
+                String.format(gameParamLocator, JSONReader.getTestDataJSON("origPriceLoc")))).getText();
+    }
+
+    public int getGameSale(){
+        return StringUtil.getIntValue(game.findElement(By.xpath(
+                String.format(gameParamLocator, JSONReader.getTestDataJSON("discount_pct")))).getText());
+    }
+
+    public String getGameFinalPrice(){
+        return game.findElement(By.xpath(
+                String.format(gameParamLocator, JSONReader.getTestDataJSON("final_price")))).getText();
+    }
+
+    public void openInstallPage(){
+        installLn.click();
+    }
 
 }
